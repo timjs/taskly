06489516b9fc613d9c7d8f17498aa2421eacdcd7
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2016-11-21 17:35:22 +0100

    restyle javascript to confirm to js-standard

--- blocks.js
+++ blocks.js
@@ -1,142 +1,157 @@
+/*global Blockly*/
+
 Blockly.Blocks['task_parallel'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("In parallel");
-    this.appendStatementInput("TASK1")
-        .setCheck("Task")
-        .appendField("do");
+        .appendField('Run in parallel')
+    this.appendStatementInput('TASK1')
+        .setCheck('Task')
+        .appendField('do')
     this.appendDummyInput()
-        .appendField("use result")
-        .appendField(new Blockly.FieldCheckbox("TRUE"), "USE1");
-    this.appendStatementInput("TASK2")
-        .setCheck("Task")
-        .appendField("do");
+        .appendField('use result')
+        .appendField(new Blockly.FieldCheckbox('TRUE'), 'USE1')
+    this.appendStatementInput('TASK2')
+        .setCheck('Task')
+        .appendField('do')
     this.appendDummyInput()
-        .appendField("use result")
-        .appendField(new Blockly.FieldCheckbox("TRUE"), "USE2");
-    this.setPreviousStatement(true, "Task");
-    this.setNextStatement(true, "Task");
-    this.setColour(290);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('use result')
+        .appendField(new Blockly.FieldCheckbox('TRUE'), 'USE2')
+    this.setPreviousStatement(true, 'Task')
+    this.setNextStatement(true, 'Task')
+    this.setColour(290)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
+
+Blockly.Blocks['task_return'] = {
+  init: function () {
+    this.appendDummyInput()
+        .appendField('return')
+        .appendField(new Blockly.FieldTextInput('value'), 'VALUE')
+    this.setPreviousStatement(true, 'Task')
+    this.setNextStatement(true, 'Task')
+    this.setColour(65)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
+  }
+}
 
 Blockly.Blocks['task_choice'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("Choice of");
-    this.appendValueInput("PREDICATE1")
-        .setCheck("Predicate")
-        .appendField("action")
-        .appendField(new Blockly.FieldTextInput("Continue"), "ACTION1")
-        .appendField("when value");
-    this.appendStatementInput("TASK1")
-        .setCheck("Task");
-    this.appendValueInput("PREDICATE2")
-        .setCheck("Predicate")
-        .appendField("action")
-        .appendField(new Blockly.FieldTextInput("Stop"), "ACTION2")
-        .appendField("when value");
-    this.appendStatementInput("TASK2")
-        .setCheck("Task");
-    this.setPreviousStatement(true, "Task");
-    this.setNextStatement(true, "Task");
-    this.setColour(210);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('Choice between')
+    this.appendValueInput('PREDICATE1')
+        .setCheck('Predicate')
+        .appendField('action')
+        .appendField(new Blockly.FieldTextInput('Continue'), 'ACTION1')
+        .appendField('when value')
+    this.appendStatementInput('TASK1')
+        .setCheck('Task')
+    this.appendValueInput('PREDICATE2')
+        .setCheck('Predicate')
+        .appendField('action')
+        .appendField(new Blockly.FieldTextInput('Stop'), 'ACTION2')
+        .appendField('when value')
+    this.appendStatementInput('TASK2')
+        .setCheck('Task')
+    this.setPreviousStatement(true, 'Task')
+    this.setNextStatement(true, 'Task')
+    this.setColour(210)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_forget'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("Forget previous result");
-    this.setPreviousStatement(true, "Task");
-    this.setNextStatement(true, "Task");
-    this.setColour(65);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('Forget previous result')
+    this.setPreviousStatement(true, 'Task')
+    this.setNextStatement(true, 'Task')
+    this.setColour(65)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_predicate_isstable'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("is stable");
-    this.setOutput(true, "Predicate");
-    this.setColour(210);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('is stable')
+    this.setOutput(true, 'Predicate')
+    this.setColour(210)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_define'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("Task")
-        .appendField(new Blockly.FieldTextInput("doSomething"), "NAME");
-    this.appendStatementInput("NAME")
-        .setCheck("Task");
-    this.setColour(120);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('Task')
+        .appendField(new Blockly.FieldTextInput('doSomething'), 'NAME')
+    this.appendStatementInput('NAME')
+        .setCheck('Task')
+    this.setColour(120)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_predicate_satisfies'] = {
-  init: function() {
-    this.appendValueInput("NAME")
-        .setCheck("Check")
-        .appendField("satisfies");
-    this.setOutput(true, "Predicate");
-    this.setColour(210);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+  init: function () {
+    this.appendDummyInput()
+        .appendField('satisfies')
+        .appendField(new Blockly.FieldTextInput('predicate'), 'PREDICATE')
+    this.setOutput(true, 'Predicate')
+    this.setColour(210)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_name'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldTextInput("doSomething"), "TASK_NAME");
-    this.setPreviousStatement(true, "Task");
-    this.setNextStatement(true, "Task");
-    this.setColour(65);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField(new Blockly.FieldTextInput('doSomething'), 'TASK_NAME')
+    this.setPreviousStatement(true, 'Task')
+    this.setNextStatement(true, 'Task')
+    this.setColour(65)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_predicate_name'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldTextInput("check"), "PREDICATE_NAME");
-    this.setOutput(true, "Check");
-    this.setColour(210);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField(new Blockly.FieldTextInput('check'), 'PREDICATE_NAME')
+    this.setOutput(true, 'Check')
+    this.setColour(210)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_predicate_isunstable'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("is unstable");
-    this.setOutput(true, null);
-    this.setColour(210);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('is unstable')
+    this.setOutput(true, null)
+    this.setColour(210)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
 
 Blockly.Blocks['task_predicate_isthereornot'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("is there or not");
-    this.setOutput(true, null);
-    this.setColour(210);
-    this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+        .appendField('is there or not')
+    this.setOutput(true, null)
+    this.setColour(210)
+    this.setTooltip('')
+    this.setHelpUrl('http://www.example.com/')
   }
-};
+}
